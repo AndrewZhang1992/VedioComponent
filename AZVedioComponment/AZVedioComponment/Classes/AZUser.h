c7b0f57c3d2b6d4713eab7ce4c4576687246bc21
Andrew_Zhang <zhanglingyun1992@163.com>
2017-06-11 17:43:40 +0800

    add user

---
+++ AZVedioComponment/AZVedioComponment/Classes/AZUser.h
@@ -0,0 +1,15 @@
+//
+//  AZUser.h
+//  AZVedioComponment
+//
+//  Created by AndrewZhang on 2017/6/11.
+//  Copyright © 2017年 AndrewZhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AZUser : NSObject
+
+@property (nonatomic, copy) NSString *name;
+
+@end
---
+++ AZVedioComponment/AZVedioComponment/Classes/AZUser.m
@@ -0,0 +1,13 @@
+//
+//  AZUser.m
+//  AZVedioComponment
+//
+//  Created by AndrewZhang on 2017/6/11.
+//  Copyright © 2017年 AndrewZhang. All rights reserved.
+//
+
+#import "AZUser.h"
+
+@implementation AZUser
+
+@end
